b48f6c8a188fd71fb0fcbe7fb9a041c5e474c572
lilvina <davinaleo2007@yahoo.com>
2019-06-13 15:09:49 -0700

    most tests are passing except for delete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,33 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.insert(len(self.storage), value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    element = self.storage
+    element[0], element[len(element) - 1] = element[len(element) - 1], element[0]
+    j = element.pop(len(element) - 1)
+    self._sift_down(0)
+    return j
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    element = self.storage
+    b = ((index - 1) // 2)
+    if b >= 0:
+        if element[index] > element[b]:
+            element[index], element[b] = element[b], element[index]
+            self._bubble_up(b)
+        else:
+            return
+    else:
+        return
 
   def _sift_down(self, index):
     pass
